2fc2a983f06ea90fcc1dcd87839080d584b17517
etquanon <etquanon@gmail.com>
2016-01-31 23:31:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var helloGit = 'Hello GIT!';
-var modified = 'A modified variable called';
+var helloGit = 'Hello GIT from MASTER!';
+var modified = 'A modified variable called in the master branch';
 
 console.log(helloGit);
 console.log(modified);
